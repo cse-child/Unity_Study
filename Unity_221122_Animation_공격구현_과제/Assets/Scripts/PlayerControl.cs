f01465da5b62645a23575aa3735889aa876cd189
cse-child <99868973+cse-child@users.noreply.github.com>
2022-11-23 00:02:00 +0900

    # 221122 과제

---
+++ Unity_221122_Animation_공격구현_과제/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float rotSpeed = 1.0f;
+
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+    private float mouseX = 0.0f;
+
+    private Vector3 direction;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        
+        direction = Vector3.right * horizontal + Vector3.forward * vertical;
+
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotSpeed * mouseX * Time.deltaTime);
+
+        if (horizontal != 0.0f || vertical != 0.0f)
+            animator.SetBool("IsRun", true);
+        else
+            animator.SetBool("IsRun", false);
+
+        if (Input.GetKey(KeyCode.Space))
+            animator.SetTrigger("IsKick");
+        
+        /*
+        if(Input.GetKey(KeyCode.W))
+        {
+            Vector3 pos = transform.position;
+            pos += transform.forward * moveSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -1) * moveSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            Vector3 rot = transform.rotation.eulerAngles;
+            rot.y += rotSpeed * Time.deltaTime;
+            //Quaternion temp = Quaternion.identity;            
+            Quaternion temp = new Quaternion();
+            temp.eulerAngles = rot;
+            transform.rotation = temp;            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * rotSpeed * Time.deltaTime);
+        }
+        */
+    }
+}
