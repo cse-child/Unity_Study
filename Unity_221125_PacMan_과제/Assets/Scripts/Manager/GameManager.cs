ff17b21cd825c99ff469f0be1cc6e3b6eacbefe0
cse-child <99868973+cse-child@users.noreply.github.com>
2022-11-27 23:00:43 +0900

    # 221127 과제

---
+++ Unity_221125_PacMan_과제/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,72 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum ObjectType
+    {
+        NONE, WALL, MONSTER, PLAYER_START
+    }
+
+    public CinemachineVirtualCamera virtualCamera;
+
+    private GameObject coinPrefab;
+    private GameObject wallPrefab;
+
+    private GameObject playerObject;
+
+    private void Awake()
+    {
+        DataManager.instance.LoadMapData();
+
+        coinPrefab = Resources.Load<GameObject>("Prefabs/Coin");
+        wallPrefab = Resources.Load<GameObject>("Prefabs/Wall");
+
+        CreateStage();
+    }
+
+    private void CreateStage()
+    {
+        List<List<int>> mapData = DataManager.instance.mapData;
+
+        for(int i = 0; i < mapData.Count; i++)
+        {
+            for(int j = 0; j < mapData[i].Count; j++)
+            {
+                ObjectType type = (ObjectType)mapData[i][j];
+
+                CreateObject(type, j, i);
+            }
+        }
+    }
+
+    private void CreateObject(ObjectType type, int x, int y)
+    {
+        Vector3 pos = new Vector3(x, 0, y);
+        switch (type)
+        {
+            case ObjectType.WALL:
+                // Instantiate: 하이어라키에 할당
+                // Quaternion.identity: 0벡터로 transform 주는거
+                Instantiate(wallPrefab, pos, Quaternion.identity);
+                break;
+            case ObjectType.PLAYER_START:
+            {
+                GameObject playerPrefab = Resources.Load<GameObject>("Prefabs/Jean");
+                playerObject = Instantiate(playerPrefab, pos, Quaternion.identity);
+                virtualCamera.Follow = playerObject.transform;
+            }
+                break;
+        }
+
+        if(type != ObjectType.WALL)
+        {
+            if (type == ObjectType.PLAYER_START)
+                return;
+
+            Instantiate(coinPrefab, pos, Quaternion.identity);
+        }
+    }
+}
---
+++ Unity_221125_PacMan_과제/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
